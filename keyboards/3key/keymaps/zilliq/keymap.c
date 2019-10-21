e5c094a61bed3d8787128e4e71d1042ea5d1800f
William O <zilliqman@gmail.com>
2019-10-21 23:55:33 +0200

    change rgb ligting with layers

--- keyboards/3key/keymaps/zilliq/keymap.c
+++ keyboards/3key/keymaps/zilliq/keymap.c
@@ -15,11 +15,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |   1   |   2   |   3   |
      * `-----------------------'
      */
-    [0] = LAYOUT(RGB_MODE_RAINBOW, RGB_TOG, RGB_MODE_XMAS),
+    [0] = LAYOUT(MO(1), MO(2), MO(3)),
+    [1] = LAYOUT(RGBLIGHT_MODE_STATIC_LIGHT, RGBLIGHT_MODE_RGB_TEST, RGBLIGHT_MODE_KNIGHT),
+    [2] = LAYOUT(RGBLIGHT_MODE_STATIC_LIGHT, RGBLIGHT_MODE_RGB_TEST, RGBLIGHT_MODE_KNIGHT),
+    [3] = LAYOUT(RGBLIGHT_MODE_STATIC_LIGHT, RGBLIGHT_MODE_RGB_TEST, RGBLIGHT_MODE_KNIGHT),
 };
 
+uint8_t prev = 0;
+
 void matrix_init_user(void){
-    //rgblight_sethsv_at(0xFF, 0x00, 0x00, 0);
-    rgblight_set_blue;
-    // rgblight_sethsv(0xFF, 0xFF, 0xEE);
+    rgblight_mode(9);
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+    uint8_t layer = biton32(state);
+    if (prev == 0) {
+        switch (layer) {
+            case 0:
+                rgblight_mode(RGBLIGHT_MODE_RGB_TEST);
+                break;
+
+            case 1:
+                rgblight_mode(RGBLIGHT_MODE_KNIGHT);
+                break;
+
+            case 2:
+                rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+                break;
+
+            case 3:
+                rgblight_setrgb_at(0, 0, 255, 0);
+                rgblight_setrgb_at(255, 255, 255, 1);
+                rgblight_setrgb_at(255, 0, 0x00, 2);
+                break;
+        }
+    }
+    prev = layer;
+    return state;
 }
